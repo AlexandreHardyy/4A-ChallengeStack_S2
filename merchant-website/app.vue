e68b9d8c99c8cdff8c8a1d2a59412fa16de48215
Théo HERVÉ <theojherve@gmail.com>
2023-07-27 16:34:24 +0200

    orders + refund in marchant website working

--- merchant-website/app.vue
+++ merchant-website/app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <Toast />
+    <ConfirmPopup></ConfirmPopup>
+    <ConfirmDialog></ConfirmDialog>
     <NuxtPage />
   </div>
 </template>
+<script setup>
+  import Toast from 'primevue/toast';
+  import ConfirmPopup from 'primevue/confirmpopup';
+  import ConfirmDialog from 'primevue/confirmdialog';
+</script>
