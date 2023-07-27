bd3381195774ef4bcbbe78cb19ddc8cb2a28baee
Alexandre Hardy <hardyalexandre2000@gmail.com>
2023-07-27 18:47:50 +0200

    hotfix sse

--- merchant-website/nuxt.config.ts
+++ merchant-website/nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
       public: {
           apiBaseClient: process.env.BASEURL_CLIENT || 'http://localhost:3000',
           apiBaseServerMerchant: process.env.BASEURL_MERCHANT_SERVER || 'http://localhost:3009',
-          clientToken: process.env.CLIENT_TOKEN || '4b1e9e82-795d-4057-81ff-00964429f331',
+          clientToken: process.env.CLIENT_TOKEN || 'd8dee9d5-aeb7-4130-b849-c72d88b99e7f',
       }
   }
 
